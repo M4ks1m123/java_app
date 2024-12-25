a9f77dffadf93ed84c06823d4d2e9a24ae9e7e5e
M4ks1m123 <maximalian67@gmail.com>
2024-12-25 20:57:58 +0300

    added auth

---
+++ src/main/java/com/example/JavaAuth/config/MyUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.example.JavaAuth.config;
+
+import com.example.JavaAuth.entities.User;
+import com.example.JavaAuth.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByName(username);
+        return user.map(MyUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException(username + "There is no such user in REPO"));
+    }
+}
---
+++ src/main/java/com/example/JavaAuth/entities/User.java
@@ -0,0 +1,95 @@
+package com.example.JavaAuth.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private String password;
+
+    private String role;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof User)) return false;
+        final User other = (User) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$id = this.getId();
+        final Object other$id = other.getId();
+        if (this$id == null ? other$id != null : !this$id.equals(other$id)) return false;
+        final Object this$name = this.getName();
+        final Object other$name = other.getName();
+        if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
+        final Object this$password = this.getPassword();
+        final Object other$password = other.getPassword();
+        if (this$password == null ? other$password != null : !this$password.equals(other$password)) return false;
+        final Object this$role = this.getRole();
+        final Object other$role = other.getRole();
+        if (this$role == null ? other$role != null : !this$role.equals(other$role)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof User;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $id = this.getId();
+        result = result * PRIME + ($id == null ? 43 : $id.hashCode());
+        final Object $name = this.getName();
+        result = result * PRIME + ($name == null ? 43 : $name.hashCode());
+        final Object $password = this.getPassword();
+        result = result * PRIME + ($password == null ? 43 : $password.hashCode());
+        final Object $role = this.getRole();
+        result = result * PRIME + ($role == null ? 43 : $role.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "User(id=" + this.getId() + ", name=" + this.getName() + ", password=" + this.getPassword() + ", role=" + this.getRole() + ")";
+    }
+}
